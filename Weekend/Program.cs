1d485b7534629ebc8b03c79e0c847335434e03d0
semen <semen.dorohov@yandex.ru>
2023-01-20 19:18:04 +0300

    Weekend

---
+++ Weekend/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите день недели: ");
+int Number1 = Convert.ToInt32(Console.ReadLine());
+
+
+
+if (Number1 == 6 ) Console.WriteLine("Да суббота это выходной");
+if (Number1 == 7 ) Console.WriteLine("Да воскресенье это выходной");
+if (Number1 < 6 ) Console.Write ("Нет, нужно работать" );
