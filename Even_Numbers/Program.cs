3eb382c1fe6dc5d2fa378b8ba424200377ff9d49
semen <semen.dorohov@yandex.ru>
2023-01-20 19:43:34 +0300

    Even Numbers

---
+++ Even_Numbers/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+int Num = Convert.ToInt32(Console.ReadLine());
+int i = 2 ;
+
+while ( i < Num + 1 )
+{  
+if (i %2 == 0 ) 
+Console.Write(" четные " + i );
+
+ i = i+2 ;
+} 
