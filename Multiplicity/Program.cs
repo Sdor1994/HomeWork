dbadfb5b5900702827d1b69684e2ad717ff1ddc9
semen <semen.dorohov@yandex.ru>
2023-01-20 19:49:38 +0300

    Multiplicity

---
+++ Multiplicity/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число:");
+int Number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2-е число:");
+int Number2 = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+
+int MultipleNumber()
+{
+    result = Number1 % Number2;
+    return result;
+}
+
+MultipleNumber();
+
+if (result == 0) System.Console.WriteLine("Число" +(Number1)+ " кратно числу " + (Number2));
+else
+{
+    Console.WriteLine("Число "+(Number1)+" не кратно числу " +(Number2)+", остаток от деления равен " + (result));
+}
