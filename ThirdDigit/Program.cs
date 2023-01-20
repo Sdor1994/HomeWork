8d2e155a66e0c24accc58b87d2991f5a0237e929
semen <semen.dorohov@yandex.ru>
2023-01-20 19:21:49 +0300

    third digit

---
+++ ThirdDigit/Program.cs
@@ -0,0 +1,8 @@
+﻿int Number = new Random().Next(1, 1500);
+Console.WriteLine();
+Console.WriteLine($"Дано число: {Number}");
+
+if (Number / 100 > 0) Console.WriteLine($"Третьей цифрой числа {Number} является цифра {Number / 1  % 10}");
+else Console.WriteLine($"У числа {Number} нет третьей цифры");
+
+Console.WriteLine();
