d1529d7bfb16a5ff96c16d19bcf40ab0bf9ef8d6
semen <semen.dorohov@yandex.ru>
2023-01-20 19:39:00 +0300

    Square

---
+++ Square/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Первое число");
+int Num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Второе число");
+int Num2 = Convert.ToInt32(Console.ReadLine());
+
+if (Num1 == Num2 * Num2) Console.WriteLine("Да " + (Num1)+ " это квадрат "+(Num2));
+else Console.WriteLine("Нет " + (Num1)+ " это квадрат "+(Num2));
