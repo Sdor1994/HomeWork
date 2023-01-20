8af539bea6458d7833ec7da7609670241b92c9fd
semen <semen.dorohov@yandex.ru>
2023-01-20 19:54:32 +0300

    DayOfTheWeek2

--- DayOf TheWeek/Program.cs
+++ DayOf TheWeek/Program.cs
@@ -1,3 +1,10 @@
-﻿Console.WriteLine("Введите число:");
-string str = Console.ReadLine();
-Console.WriteLine("Второе Цифра:" + str[1]);
+﻿Console.WriteLine("Введите число: ");
+int Num = Convert.ToInt32(Console.ReadLine());
+
+if (Num == 1) Console.WriteLine("Поденельник");
+if (Num == 2) Console.WriteLine("Вторник");
+if (Num == 3) Console.WriteLine("Среда");
+if (Num == 4) Console.WriteLine("Четверг");
+if (Num == 5) Console.WriteLine("Пятница");
+if (Num == 6) Console.WriteLine("Суббота");
+if (Num == 7) Console.WriteLine("Воскресенье");
