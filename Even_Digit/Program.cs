cbf260d7bb112cf511616b49394eb56a54a4504e
semen <semen.dorohov@yandex.ru>
2023-01-20 19:47:23 +0300

    Even Digit

---
+++ Even_Digit/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите число: ");
+int Number1 = Convert.ToInt32(Console.ReadLine());
+
+if (Number1 %2 ==0 ) Console.WriteLine( (Number1)+" Число четное");
+else Console.WriteLine((Number1)+" Число нечетное");
