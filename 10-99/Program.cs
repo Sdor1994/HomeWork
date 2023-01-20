7040ac8f93ae13e23add66b294f4eadef9a21846
semen <semen.dorohov@yandex.ru>
2023-01-20 19:23:56 +0300

    10-99

---
+++ 10-99/Program.cs
@@ -0,0 +1,9 @@
+﻿int Number = new Random().Next(10, 100);
+
+int Max = 0;
+
+int One = Number / 10;
+int Two = Number % 10;
+if (One > Two) Max = One;
+else Max = Two;
+System.Console.WriteLine("В числе " + ( Number ) + " наибольшая цифра " + (Max));
