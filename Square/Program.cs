e369415e14ada18f5410f34547764191c03f5d61
Sdor1994 <122234785+Sdor1994@users.noreply.github.com>
2023-01-21 19:44:16 +0300

    Update Program.cs

--- Square/Program.cs
+++ Square/Program.cs
@@ -4,4 +4,4 @@ Console.WriteLine("Второе число");
 int Num2 = Convert.ToInt32(Console.ReadLine());
 
 if (Num1 == Num2 * Num2) Console.WriteLine("Да " + (Num1)+ " это квадрат "+(Num2));
-else Console.WriteLine("Нет " + (Num1)+ " это квадрат "+(Num2));
+else Console.WriteLine("Нет " + (Num1)+ " это не квадрат "+(Num2));
